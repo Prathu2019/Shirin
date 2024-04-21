b15380147a7250b0eb313d4a11aebb89c095a19a
Prathu2019 <147297724+Prathu2019@users.noreply.github.com>
2024-04-21 11:54:37 +0530

    Update Name1.java

--- Shirin/src/Name/Name1.java
+++ Shirin/src/Name/Name1.java
@@ -3,7 +3,7 @@ package Name;
 public class Name1 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Name Prathibha");
 		System.out.println("Name Shirin");
 		System.out.println("Name Yuvaan");
 	}
